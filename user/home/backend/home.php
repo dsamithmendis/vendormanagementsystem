7d6ebb5aac89a093d46c8ac76d46fb875fa84ed4
Samith Mendis <samithmendis.01@gmail.com>
2025-05-23 02:17:41 +0530

    Update home.php

--- user/home/backend/home.php
+++ user/home/backend/home.php
@@ -4,14 +4,14 @@ session_start();
 
 include '/vendormanagementsystem/user/home/index';
 include '/vendormanagementsystem/connection/connect.php';
+include '/vendormanagementsystem/verify/verifyuser.php';
+
+if (!isset($_SESSION["username"])) {
+    die("Unauthorized access.");
+}
 
 if (!$connection) {
     die("Database connection failed: " . mysqli_connect_error());
-}
-
-include '/vendormanagementsystem/verify/verifyuser.php';
-if (!isset($_SESSION["username"])) {
-    die("Unauthorized access.");
 }
 
 ob_end_flush();
